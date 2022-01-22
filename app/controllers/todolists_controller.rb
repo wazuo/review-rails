ab00a235866f7b0facdb985bedb0308de2ecb107
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2022-01-22 01:06:42 +0000

    [finish]rails6 Add edit

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -15,10 +15,17 @@ class TodolistsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    
   end
 
   def edit
     @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
 
 
